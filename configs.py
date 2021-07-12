70194266f4cc1a5252eed37ff1a14162b3a4541f
brunas-x <87294624+brunas-x@users.noreply.github.com>
2021-07-12 11:05:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -29,21 +29,6 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
-
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-👥 **Support Group:** [Linux Repositories](https://t.me/linux_repo)
-
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
-"""
-	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
-
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
